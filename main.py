df69ab2bd2b57e066935e4135819ff71afeb4fda
SarBH <saritahedaya@gmail.com>
2020-05-04 22:32:45 -0400

    added call to PatchMatch

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from patchmatch_stereo.dataset import Dataset
+from patchmatch_stereo.patchmatch import PatchMatch
 
 if __name__ == "__main__":
     data_path = "data/"
     mvs_dataset = Dataset(data_path)
-    entry_images = mvs_dataset.get_images('entry')
-    print(entry_images.head())
-    p_mats = mvs_dataset.get_p_matrices('fountain')
-    print(p_mats.head())
+
+    f_images = mvs_dataset.get_images('fountain')
+    f_p_mats = mvs_dataset.get_p_matrices('fountain')
+    f_intrinsics = mvs_dataset.get_intrinsics('fountain')
+
+    pm_fountain = PatchMatch(f_images, f_p_mats, f_intrinsics)
+
+    # pm_fountain.run_iteration()
